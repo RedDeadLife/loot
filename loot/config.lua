79cdd5a80061aaf664e9759c4d75e3b41f37d6ed
Riryan <anthonyfmitchell@gmail.com>
2020-07-04 12:36:50 -0400

    Add files via upload

--- loot/config.lua
+++ loot/config.lua
@@ -1,1 +1,7 @@
--- More todo
+lootable = {
+    [1] =  {["item"] = "Revolver bullet",["poor"] = 957520252,["good"] = 143941906,["perfect"] = 1292673537},
+    [2] =  {["item"] = "arrow",          ["poor"] = 957520252,["good"] = 143941906,["perfect"] = 1292673537},
+    [3] =  {["item"] = "Shotgun shell",  ["poor"] = 957520252,["good"] = 143941906,["perfect"] = 1292673537},
+    [4] =  {["item"] = "bullet",         ["poor"] = 957520252,["good"] = 143941906,["perfect"] = 1292673537},
+    [5] =  {["item"] = "bullet",         ["poor"] = 957520252,["good"] = 143941906,["perfect"] = 1292673537}
+}
