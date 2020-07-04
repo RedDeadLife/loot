61594dab25c09dc5504c767076e1378d8a986189
Riryan <anthonyfmitchell@gmail.com>
2020-07-04 12:36:18 -0400

    Add files via upload

--- loot/server/server.lua
+++ loot/server/server.lua
@@ -1,3 +1,4 @@
+Inventory = {}
 RegisterServerEvent('loot:addmoney')
 AddEventHandler('loot:addmoney', function(price)
 	local _source = source
@@ -9,8 +10,16 @@ RegisterServerEvent('loot:addxp')
 AddEventHandler('loot:addxp', function(xppay)
 	local _source = source
 	local _xppay = tonumber(xppay)
-	TriggerEvent("vorp:addXp", _source, 0, xppay)
-	TriggerClientEvent("vorp:TipRight", _source, 'XP Added', 3000)
+	TriggerEvent("vorp:addXp", _source, xppay)
+	TriggerClientEvent("vorp:TipRight", _source, xppay..' XP', 3000)
 end)
 
-
+RegisterServerEvent('loot:giveitem')
+AddEventHandler('loot:giveitem', function(item)
+	local _item = item
+    local _source = source
+	local randomitem =  math.random(1,5)
+	Inventory = call
+	--Inventory.addItem(_source, _item, randomitem)
+	TriggerClientEvent("vorp:TipRight", _source,"loot is comeing soon", 3000)
+end)
