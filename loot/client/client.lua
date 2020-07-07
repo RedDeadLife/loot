c8f43f43b08f1888c80c387bf8015aa14134896a
Riryan <anthonyfmitchell@gmail.com>
2020-07-07 15:58:51 -0400

    Add files via upload
    Fixed issue with being able to loot money and xp from all peds 
    now loot only works for human peds

--- loot/client/client.lua
+++ loot/client/client.lua
@@ -1,8 +1,8 @@
 local looting = false
-
+local islooted = false
 Citizen.CreateThread(function()
     while true do
-		Wait(0)
+		Wait(1)
 		if IsControlJustPressed(0,1101824977) then 
 			local area = true
 			local player = PlayerPedId()
@@ -11,38 +11,40 @@ Citizen.CreateThread(function()
 			local rtnVal, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(shapeTest)
 			local type = GetPedType(entityHit)
 			local dead = IsEntityDead(entityHit)
-			if type == 4 and dead then
-				local looted = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
-				if not looted then
-					shape = false
-					looting = true
-					PressTime = GetGameTimer()
-					while looting do
-						Wait(5)
-						if IsControlJustReleased(0,1101824977) then
-							KeyHeldTime = GetGameTimer() - PressTime
-							PressTime = 0
-								print(KeyHeldTime)
-							if KeyHeldTime > 250 then
-								looting = false
-								Wait(5)
-								local lootcheck = entityHit;
-									if lootcheck then
-										local loot = math.random(1, 10)
-										local lootpay = loot / 100
-										local xppay = math.random(1, 2)
-										local stuff = math.random(1,100)
+			local human = IsPedHuman(entityHit)
+			
+			if type == 4 or dead then
+					local looted = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
+					if not looted then
+						shape = false
+						looting = true
+						PressTime = GetGameTimer()
+						while looting do
+							Wait(5)
+							if IsControlJustReleased(0,1101824977) then
+								KeyHeldTime = GetGameTimer() - PressTime
+								PressTime = 0
+								if KeyHeldTime > 250 then
+									Wait(5)
+									local lootcheck = entityHit;
+										if lootcheck and human then
+											looting = true
+											local loot = math.random(1, 10)
+											local lootpay = loot / 100
+											local xppay = math.random(0, 2)
+											local stuff = math.random(1,100)
 											TriggerServerEvent("loot:addmoney", lootpay)
 											TriggerServerEvent("loot:addxp", xppay)
 											TriggerServerEvent("loot:giveitem", stuff)
-									else
-									looting = false
+										else
+										looting = false
+									end
+									looting = false							
 								end
-								looting = false							
 							end
 						end
 					end
-				end
+				--end
 			end
     	end
     end
