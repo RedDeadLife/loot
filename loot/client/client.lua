c411b07bcb52b97ac56b82a26d4e8f9a79af085c
Riryan <anthonyfmitchell@gmail.com>
2020-07-08 09:56:39 -0400

    Add files via upload

--- loot/client/client.lua
+++ loot/client/client.lua
@@ -1,54 +1,56 @@
+--[[Cryptos loot system Converted for VOIP Core.]]
+
+
 local looting = false
-local islooted = false
 Citizen.CreateThread(function()
     while true do
-		Wait(1)
-		if IsControlJustPressed(0,1101824977) then 
-			local area = true
-			local player = PlayerPedId()
-			local coords = GetEntityCoords(player)
-			local shapeTest = StartShapeTestBox(coords.x, coords.y, coords.z, 2.0, 2.0, 2.0, 0.0, 0.0, 0.0, true, 8, player)
-			local rtnVal, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(shapeTest)
-			local type = GetPedType(entityHit)
-			local dead = IsEntityDead(entityHit)
-			local human = IsPedHuman(entityHit)
-			
-			if type == 4 or dead then
+		Wait(0)
+		if IsControlJustPressed(0,1101824977) and not IsPedInAnyVehicle(player, true) and not looting then
+			local shape = true
+			while shape do
+				Wait(0)
+				local player = PlayerPedId()
+				local coords = GetEntityCoords(player)
+				local entityHit = 0
+				local shapeTest = StartShapeTestBox(coords.x, coords.y, coords.z, 2.0, 2.0, 2.0, 0.0, 0.0, 0.0, true, 8, player)
+				local rtnVal, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(shapeTest)
+				local type = GetPedType(entityHit)
+				local dead = IsEntityDead(entityHit)
+				local PressTime = 0
+				if type == 4 and dead then
 					local looted = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
 					if not looted then
 						shape = false
 						looting = true
 						PressTime = GetGameTimer()
 						while looting do
-							Wait(5)
+							Wait(0)
 							if IsControlJustReleased(0,1101824977) then
 								KeyHeldTime = GetGameTimer() - PressTime
 								PressTime = 0
 								if KeyHeldTime > 250 then
-									Wait(5)
-									local lootcheck = entityHit;
-										if lootcheck and human then
-											looting = true
-											local loot = math.random(1, 10)
-											local lootpay = loot / 100
-											local xppay = math.random(0, 2)
-											local stuff = math.random(1,100)
-											TriggerServerEvent("loot:addmoney", lootpay)
-											TriggerServerEvent("loot:addxp", xppay)
-											TriggerServerEvent("loot:giveitem", stuff)
-										else
+									looting = false
+									Wait(500)
+									local lootedcheck = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
+									if lootedcheck then
+										local loot = math.random(1, 10)
+										local lootpay = loot / 100
+										local xppay = math.random(0, 2)
+										local stuff = math.random(1,100)
+										TriggerServerEvent("loot:addmoney", lootpay)
+										TriggerServerEvent("loot:addxp", xppay)
+										TriggerServerEvent("loot:giveitem", stuff)
+									else
 										looting = false
 									end
-									looting = false							
+								else
+									looting = false
 								end
 							end
 						end
 					end
-				--end
+				end
 			end
-    	end
+		end
     end
 end)
-
-
-
