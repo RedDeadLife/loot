9cc2d785ca5b24426a9106216aa710824d26a9aa
Riryan <anthonyfmitchell@gmail.com>
2020-08-13 10:15:10 -0400

    Add files via upload

---
+++ loot/config.lua
@@ -0,0 +1,153 @@
+Animal ={
+  [1] =  {["name"] = "Bear",                     ["model"] = -1124266369,  ["item"] = "meat",       ["item2"] = "bearhide", ["poor"] = 957520252,   ["good"] = 143941906,   ["perfect"] = 1292673537},
+  [2] =  {["name"] = "Big Horn Ram",             ["model"] = -15687816381, ["item"] = "meat",       ["item2"] = "goathide", ["poor"] = 1796037447,  ["good"] = -476045512,  ["perfect"] = 1795984405},
+  [3] =  {["name"] = "Boar",                     ["model"] = 2028722809,   ["item"] = "meat",       ["item2"] = "boarhide", ["poor"] = 1248540072,  ["good"] = nil,         ["perfect"] = -1858513856},
+  [4] =  {["name"] = "Buck",                     ["model"] = -1963605336,  ["item"] = "meat",       ["item2"] = "deerhide", ["poor"] = 1603936352,  ["good"] = -868657362,  ["perfect"] = -702790226, },
+  [5] =  {["name"] = "Bison",                    ["model"] = 2028722809,   ["item"] = "meat",       ["item2"] = "bisonhide",["poor"] = -1730060063, ["good"] = -591117838,  ["perfect"] = -237756948  },
+  [6] =  {["name"] = "Bull",                     ["model"] = 195700131,    ["item"] = "meat",       ["item2"] = "bullskin", ["poor"] = 9293261,     ["good"] = -336086818,  ["perfect"] = -53270317  },
+  [7] =  {["name"] = "Deer",                     ["model"] = 1110710183,   ["item"] = "meat",       ["item2"] = "deerhide", ["poor"] = -662178186,  ["good"] = -1827027577, ["perfect"] = -1035515486  },
+  [8] =  {["name"] = "Elk",                      ["model"] = -2021043433,  ["item"] = "meat",       ["item2"] = "elkhide",  ["poor"] = 2053771712,  ["good"] = 1181652728,  ["perfect"] = -1332163079, ["reward"] = 5,  },
+  [9] =  {["name"] = "American Red Fox",         ["model"] = 252669332,    ["item"] = "gameymeat",  ["item2"] = "foxpelt",  ["poor"] = 1647012424,  ["good"] = 238733925,   ["perfect"] = 500722008,   ["reward"] = 5,  },
+  [10] = {["name"] = "Big Grey Wolf",            ["model"] = -1143398950,  ["item"] = "gameymeat",  ["item2"] = "lggreywolfskin",        ["poor"] = 85441452,    ["good"] = 1145777975,  ["perfect"] = 653400939,   ["reward"] = 6,  },
+  [11] = {["name"] = "Medium Grey Wolf",         ["model"] = -885451903,   ["item"] = "gameymeat",  ["item2"] = "greywlfskin",        ["poor"] = 85441452,    ["good"] = 1145777975,  ["perfect"] = 653400939,   ["reward"] = 5,  },
+  [12] = {["name"] = "Small Grey Wolf",          ["model"] = -829273561,   ["item"] = "gameymeat",  ["item2"] = "smgreywolfskin",        ["poor"] = 85441452,    ["good"] = 1145777975,  ["perfect"] = 653400939,   ["reward"] = 4,  },
+  [13] = {["name"] = "Snapping Turtle",          ["model"] = -407730502,   ["item"] = "meat",       ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 3,  },
+  [14] = {["name"] = "Water Snake",              ["model"] = -229688157,   ["item"] = "snakeskin",  ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 3,  },
+  [15] = {["name"] = "Water Snake",              ["model"] = -229688157,   ["item"] = "snakeskin",  ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 3,  },
+  [16] = {["name"] = "Snake Red Boa",            ["model"] = -1790499186,  ["item"] = "snakeskin",  ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 6,  },
+  [17] = {["name"] = "Snake Fer-De-Lance",       ["model"] = 1464167925,   ["item"] = "snakeskin",  ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 6,  },
+  [18] = {["name"] = "Black-Tailed Rattlesnake", ["model"] = 846659001,    ["item"] = "snakeskin",  ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 6,  },
+  [19] = {["name"] = "Western Rattlesnake",      ["model"] = 545068538,    ["item"] = "snakeskin",  ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 6,  },
+  [20] = {["name"] = "Striped Skunk",            ["model"] = -1211566332,  ["item"] = "meat",       ["item2"] = "skunkpelt",["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 3,  },
+  [21] = {["name"] = "Merino Sheep",             ["model"] = 40345436,     ["item"] = "mutton",     ["item2"] = nil,        ["poor"] = 1729948479,  ["good"] = -1317365569, ["perfect"] = 1466150167,  ["reward"] = 1,  },
+  [22] = {["name"] = "American Pronghorn Doe",   ["model"] = 1755643085,   ["item"] = "meat",       ["item2"] = nil,        ["poor"] = -983605026,  ["good"] = 554578289,   ["perfect"] = -1544126829, ["reward"] = 1,  },
+  [23] = {["name"] = "Wirginia Possum",          ["model"] = -1414989025,  ["item"] = "meat",       ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 2,  },
+  [24] = {["name"] = "Berkshire Pig",            ["model"] = 1007418994,   ["item"] = "meat",       ["item2"] = nil,        ["poor"] = -308965548,  ["good"] = -57190831,   ["perfect"] = -1102272634, ["reward"] = 1,  },
+  [25] = {["name"] = "Panther",                  ["model"] = 1654513481,   ["item"] = "meat",       ["item2"] = nil,        ["poor"] = 1584468323,  ["good"] = -395646254,  ["perfect"] = 1969175294,  ["reward"] = 6,  },
+  [26] = {["name"] = "Cougar",                   ["model"] = 90264823,     ["item"] = "meat",       ["item2"] = nil,        ["poor"] = 1914602340,  ["good"] = 459744337,   ["perfect"] = -1791452194, ["reward"] = 6,  },
+  [27] = {["name"] = "Florida Cracker Cow",      ["model"] = -50684386,    ["item"] = "meat",       ["item2"] = nil,        ["poor"] = 334093551,   ["good"] = 1150594075,  ["perfect"] = -845037222,  ["reward"] = 1,  },
+  [28] = {["name"] = "Coyote",                   ["model"] = 480688259,    ["item"] = "meat",       ["item2"] = nil,        ["poor"] = -1558096473, ["good"] = 1150939141,  ["perfect"] = -794277189,  ["reward"] = 2,  },
+  [29] = {["name"] = "Gila Monster",             ["model"] = 457416415,    ["item"] = "meat",       ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 3,  }, -- lizard
+  [30] = {["name"] = "Alpine Goat",              ["model"] = -753902995,   ["item"] = "meat",       ["item2"] = nil,        ["poor"] = 699990316,   ["good"] = 1710714415,  ["perfect"] = -1648383828, ["reward"] = 2,  },
+  [31] = {["name"] = "Green Iguana",             ["model"] = -1854059305,  ["item"] = "meat",       ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 3,  },
+  [32] = {["name"] = "Desert Iguana",            ["model"] = -593056309,   ["item"] = "meat",       ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 3,  },
+  [33] = {["name"] = "Peccary Pig",              ["model"] = 1751700893,   ["item"] = "meat",       ["item2"] = nil,        ["poor"] = -99092070,   ["good"] = -1379330323, ["perfect"] = 1963510418,  ["reward"] = 1,  }, -- type of boar (javelina), dont know if its a legendary or not
+  [34] = {["name"] = "Moose",                    ["model"] = -1098441944,  ["item"] = "meat",       ["item2"] = nil,        ["poor"] = 1868576868,  ["good"] = 1636891382,  ["perfect"] = -217731719,  ["reward"] = 5,  },
+  [35] = {["name"] = "American Muskrat",         ["model"] = -1134449699,  ["item"] = "meat",       ["item2"] = nil,        ["poor"] = nil,         ["good"] = nil,         ["perfect"] = nil,         ["reward"] = 2,  },
+  [36] = {["name"] = "Angus Ox",                 ["model"] = 556355544,    ["item"] = "meat",       ["item2"] = nil,        ["poor"] = 4623248928,  ["good"] = 1208128650,  ["perfect"] = 659601266,   ["reward"] = 1,  },
+  [37] = {["name"] = "Alligator",                ["model"] = -1892280447,  ["item"] = "meat",       ["item2"] = nil,        ["poor"] = 1806153689,  ["good"] = -802026654,  ["perfect"] = -1625078531, ["reward"] = 6,  },
+  [38] = {["name"] = "North American Beaver",    ["model"] = 759906147,    ["item"] = "meat",       ["item2"] = nil,        ["poor"] = -1569450319, ["good"] = -2059726619, ["perfect"] = 854596618,   ["reward"] = 2,  },
+  [39] = {["name"] = "American Black Bear",      ["model"] = 730092646,    ["item"] = "meat",       ["item2"] = nil,        ["poor"] = 1083865179,  ["good"] = 1490032862,  ["perfect"] = 663376218,   ["reward"] = 7,  },
+  [40] = {["name"] = "American Alligator",       ["model"] = -1892280447,  ["item"] = "meat",       ["item2"] = nil,        ["poor"] = 1806153689,  ["good"] = -802026654,  ["perfect"] = -1625078531, ["reward"] = 6,  },
+  [41] = {["name"] = "California Vally Coyote",  ["model"] = 480688259,    ["item"] = "meat",       ["item2"] = nil,        ["poor"] = -1558096473, ["good"] = 1150939141,  ["perfect"] = -794277189,  ["reward"] = 2,  },
+  [42] = {["name"] = "WhiteTail Deer",           ["model"] = 1110710183,   ["item"] = "meat",       ["item2"] = "deerskin",        ["poor"] = -662178186,  ["good"] = -1827027577, ["perfect"] = -1035515486, ["reward"] = 1,  },
+ 
+}
+  Collectables = {
+   -- [1] = {["name"] = "penny",                   ["model"] = 00000000,     ["item"] = nil,          ["x"]= 0, ["y"]= 0, ["z"]=0},
+ }
+ Critter = {
+   [1] =  {["name"] = "Scarlet Tanager Songbird",["model"] = -1910795227,   ["item"] = "songbirdcarcass",      ["item2"] = nil},--
+   [2] =  {["name"] = "Woodpecker",              ["model"] = 000001,        ["item"] = "woodpeckercarcass",      ["item2"] = nil},
+   [3] =  {["name"] = "Bullfrog",                ["model"] = -930822792,    ["item"] = "frogleg" ,               ["item2"] = nil},--
+   [4] =  {["name"] = "Blue Jay" ,               ["model"] = 000000,        ["item"] = "bluejaycarcass",         ["item2"] = nil},--
+   [5] =  {["name"] = "Cardinal" ,               ["model"] = 000000,        ["item"] = "cardinalcarcass",        ["item2"] = nil},--
+   [6] =  {["name"] = "Egret",                   ["model"] = 831859211,     ["item"] = "egretcarcass",           ["item2"] = nil},--
+   [7] =  {["name"] = "Carolina Parakeet",       ["model"] = 000000,        ["item"] = "carolinaparakeetcarcass",["item2"] = nil},--
+   [8] =  {["name"] = "Cedar Waxwing",           ["model"] = 000000,        ["item"] = "cedarwaxwingcarcass",    ["item2"] = nil},--
+   [9] =  {["name"] = "Chipmunk",                ["model"] = 000000,        ["item"] = "chipmunkcarcass",        ["item2"] = nil},--
+   [10] = {["name"] = "Crab",                   ["model"] = 000000,        ["item"] = "crabmeat",               ["item2"] = nil},--
+   [13] = {["name"] = "Cormorant",              ["model"] = 000000,        ["item"] = "carmarantcarcass",       ["item2"] = nil},
+   [14] = {["name"] = "Crawfish",               ["model"] = 000000,        ["item"] = "crawfish",               ["item2"] = nil},--
+   [15] = {["name"] = "Crow",                   ["model"] = 1095117488,    ["item"] = "crowcarcass",            ["item2"] = nil},--
+   [16] = {["name"] = "Brown Rat",              ["model"] = -930822792,    ["item"] = "brownratcarcass",        ["item2"] = nil},--
+   [17] = {["name"] = "Black-Tailed Jackrabbit",["model"] = 563041215,     ["item"] = "rabbithide",             ["item2"] = nil},--
+   [18] = {["name"] = "Blacksquirrel",          ["model"] = 1465438313,    ["item"] = "blacksquirrelcarcass",   ["item2"] = nil},--
+   [19] = {["name"] = "American Crow",          ["model"] = 1095117488,    ["item"] = "crowcarcass",            ["item2"] = nil},--
+   [20] = {["name"] = "North American Racoon",  ["model"] = 1458540991,    ["item"] = "racoonhide",             ["item2"] = nil},--
+   [21] = {["name"] = "American Red Squirrel",  ["model"] = 1465438313,    ["item"] = nil,                      ["item2"] = nil},
+}
+Fowl = {
+  [1] =  {["name"] = "Chicken",                  ["model"] = 000000,       ["item"] = "feather",       ["item2"] = nil},
+  [2] =  {["name"] = "Common Loon",              ["model"] = 386506078,    ["item"] = "feather",       ["item2"] = nil},
+  [3] =  {["name"] = "Great Horned Owl",         ["model"] = -861544272,   ["item"] = "feather",       ["item2"] = nil},
+  [4] =  {["name"] = "Whooping Crane",           ["model"] = -564099192,   ["item"] = "feather",       ["item2"] = nil},  
+  [5] =  {["name"] = "Western Turkey Vulture",   ["model"] = 1104697660,   ["item"] = "feather",       ["item2"] = nil},
+  [6] =  {["name"] = "Eagle",                    ["model"] = 1459778951,   ["item"] = "feather",       ["item2"] = nil},
+  [7] =  {["name"] = "Ferruinous Hawk",          ["model"] = -2145890973,  ["item"] = "feather",       ["item2"] = nil},
+  [8] =  {["name"] = "Duck",                     ["model"] = -1003616053,  ["item"] = "feather",       ["item2"] = nil},
+  [9] =  {["name"] = "Canada Goose",             ["model"] = 723190474,    ["item"] = "feather",       ["item2"] = nil},
+  [10] = {["name"] = "Great Blue Heron",         ["model"] = 1095117488,   ["item"] = "feather",       ["item2"] = nil},
+  [11] = {["name"] = "Californian Condor",       ["model"] = 1205982615,   ["item"] = "feather",       ["item2"] = nil},
+  [12] = {["name"] = "Dominique Chicken",        ["model"] = -2063183075,  ["item"] = "feather",       ["item2"] = nil},
+  [13] = {["name"] = "Double-Crested Cormorant", ["model"] = -2073130256,  ["item"] = "feather",       ["item2"] = nil},
+  [14] = {["name"] = "Ring-Necked Pheasant",     ["model"] = 1416324601,   ["item"] = "feather",       ["item2"] = nil},
+  [15] = {["name"] = "American White Pelican",   ["model"] = 1265966684,   ["item"] = "feather",       ["item2"] = nil},
+  [16] = {["name"] = "Blue And Yellow Macaw",    ["model"] = -1797450568,  ["item"] = "feather",       ["item2"] = nil},
+  [17] = {["name"] = "Greater Prairie Chicken",  ["model"] = 2079703102,   ["item"] = "feather",       ["item2"] = nil},
+  [18] = {["name"] = "Herring Seagull",          ["model"] = -164963696,   ["item"] = "feather",       ["item2"] = nil},
+  [19] = {["name"] = "Roseate Spoonbill",        ["model"] = -1076508705,  ["item"] = "feather",       ["item2"] = nil},
+  [20] = {["name"] = "Dominique Rooster",        ["model"] = 2023522846,   ["item"] = "feather",       ["item2"] = nil},
+  [21] = {["name"] = "Red-Footed Booby",         ["model"] = -466687768,   ["item"] = "feather",       ["item2"] = nil},
+  [22] = {["name"] = "Wester Raven",             ["model"] = -575340245,   ["item"] = "feather",       ["item2"] = nil},
+  [23] = {["name"] = "Wild Turkey",              ["model"] = -466054788,   ["item"] = "feather",       ["item2"] = nil},
+  [24] = {["name"] = "Wild Turkey",              ["model"] = -2011226991,  ["item"] = "feather",       ["item2"] = nil},
+  [25] = {["name"] = "Wild Turkey",              ["model"] = -166054593,   ["item"] = "feather",       ["item2"] = nil},
+  [26] = {["name"] = "Vulture",                  ["model"] = 1104697660,   ["item"] = "feather",       ["item2"] = nil},
+  
+}
+
+ Loot = {
+   [1] =  {["number"] = 1,  ["item1"]= nil},
+   [2] =  {["number"] = 2,  ["item1"]= nil},
+   [3] =  {["number"] = 3,  ["item1"]= nil},
+   [4] =  {["number"] = 4,  ["item1"]= nil},
+   [5] =  {["number"] = 5,  ["item1"]= nil},
+   [6] =  {["number"] = 6,  ["item1"]= nil},
+   [7] =  {["number"] = 7,  ["item1"]= nil},
+   [8] =  {["number"] = 8,  ["item1"]= nil},
+   [9] =  {["number"] = 9,  ["item1"]= nil},
+   [10] = {["number"] = 10, ["item1"]= nil},
+   [11] = {["number"] = 11, ["item1"]= nil},
+   [12] = {["number"] = 12, ["item1"]= nil},
+   [13] = {["number"] = 13, ["item1"]= nil},
+   [14] = {["number"] = 14, ["item1"]= nil},
+   [15] = {["number"] = 15, ["item1"]= nil},
+   [16] = {["number"] = 16, ["item1"]= nil},
+   [17] = {["number"] = 17, ["item1"]= nil},
+   [18] = {["number"] = 18, ["item1"]= nil},
+   [19] = {["number"] = 19, ["item1"]= nil},
+   [20] = {["number"] = 20, ["item1"]= nil},
+   [21] = {["number"] = 21, ["item1"]= nil},
+   [22] = {["number"] = 22, ["item1"]= nil},
+   [23] = {["number"] = 23, ["item1"]= nil},
+   [24] = {["number"] = 24, ["item1"]= nil},
+   [25] = {["number"] = 25, ["item1"]= nil},
+   [26] = {["number"] = 26, ["item1"]= nil},
+   [27] = {["number"] = 27, ["item1"]= nil},
+   [28] = {["number"] = 28, ["item1"]= nil},
+   [29] = {["number"] = 29, ["item1"]= nil},
+   [30] = {["number"] = 30, ["item1"]= nil},
+   [31] = {["number"] = 31, ["item1"]= nil},
+   [32] = {["number"] = 32, ["item1"]= nil},
+   [33] = {["number"] = 33, ["item1"]= nil},
+   [34] = {["number"] = 34, ["item1"]= nil},
+   [35] = {["number"] = 35, ["item1"]= nil},
+   [36] = {["number"] = 36, ["item1"]= nil},
+   [37] = {["number"] = 37, ["item1"]= nil},
+   [38] = {["number"] = 38, ["item1"]= nil},
+   [39] = {["number"] = 39, ["item1"]= nil},
+   [40] = {["number"] = 40, ["item1"]= nil},
+   [41] = {["number"] = 41, ["item1"]= nil},
+   [42] = {["number"] = 42, ["item1"]= nil},
+   [43] = {["number"] = 43, ["item1"]= nil},
+   [44] = {["number"] = 44, ["item1"]= nil},
+   [45] = {["number"] = 45, ["item1"]= nil},
+   [46] = {["number"] = 46, ["item1"]= nil},
+   [47] = {["number"] = 47, ["item1"]= nil},
+   [48] = {["number"] = 48, ["item1"]= nil},
+   [49] = {["number"] = 49, ["item1"]= nil},
+   [50] = {["number"] = 50, ["item1"]= nil},
+
+    
+ }
